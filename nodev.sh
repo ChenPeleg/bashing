3eae1658356a165c31203a5c65df777df4394aa1
chenp <chenpeleg@gmail.com>
2022-09-09 10:40:36 +0300

    added nvmrc

---
+++ nodev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dubug="false"
+
+
+nodeVersionFileName="./src/app/Services/versions/app.version.json"
+
+echo "Cheching node version..."
+
+text1="you choose ${a1}"
+echo "${text1}" 
+a2="1"
+
+if [ "$a1" == "$a2" ]; then
+	echo "dir was saved in file"
+elif [ "$a1" -eq "2" ]; then
+	ls -lh
+fi
+
+
+ 
+
+
+
